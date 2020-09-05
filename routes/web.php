a1f8c017acf1d34dd9a417f15120a83221cf84c4
David Buttler <david.buttler@team.aussiebroadband.com.au>
2020-09-05 17:20:02 +1000

    Rename welcome file and create routes and controllers for login and register functions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Login
+Route::get('/', function () { return view('login'); });
+Route::post('/login', [LoginController::class, 'login']);
+
+// Register
+Route::get('/user/register', function() { return view('register'); });
+Route::post('/user/create', [RegisterController::class, 'register']);
