56dae1c5c014a6d269f0d0171c139e4551ef50c5
David Buttler <david.buttler@team.aussiebroadband.com.au>
2020-09-06 14:27:21 +1000

    Created logic behind registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class RegisterController extends Controller
 {
-    public function register(): void
+    public function register(Request $request): View
     {
+        $success = false;
+        $user = new User();
+        $email = $request->input('email');
+        $password = $request->input('password');
 
+        if ($email && $password) {
+            // Sanitise user input.
+            $sanitisedEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+            $sanitisedPassword = filter_var($password, FILTER_SANITIZE_STRING);
+            $hashedPassword = password_hash($sanitisedPassword, PASSWORD_DEFAULT);
+
+            // User can't register if they have an account obviously.
+            if (empty($user->getOne($sanitisedEmail))) {
+                $name = $request->input('name');
+
+                $user->name = $name;
+                $user->email = $sanitisedEmail;
+                $user->password = $hashedPassword;
+                $user->save();
+
+                return view('register-success', ['name' => $name]);
+            } else {
+                return view('register', ['error' => 'An account with email provided already exists']);
+            }
+        }
     }
 }
